520d1bbbeffcb65e9fb9de6a26ac94a43807b401
Xron Trix <xrontrix@gmail.com>
2024-02-28 05:10:25 +0530

    Added fully Functional Todo Page

---
+++ src/app/todos/TodoLists.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState } from "react";
+import { Todo } from "@/lib/types";
+import { Trash2 } from "lucide-react";
+
+const TodoLists = (props: { todoList: Todo[] }) => {
+  const [list, setList] = useState<Todo[]>(props.todoList);
+
+  const handleCheck = (id: number) => {
+    const updatedList = list.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setList(updatedList);
+  };
+
+  const handleDelete = (id: number) => {
+    const updatedList = list.filter((todo) => todo.id !== id);
+    setList(updatedList);
+  };
+
+  return (
+    <div className="flex flex-col gap-6">
+      {list.map((todo) => (
+        <div
+          key={todo.id}
+          className="flex items-center justify-between py-3 px-5 border border-amber-400 bg-gradient-to-r from-amber-200 via-gray-400 to-amber-100 rounded-full"
+        >
+          <div className="flex items-center gap-4">
+            <input
+              type="checkbox"
+              onClick={() => handleCheck(todo.id)}
+              checked={todo.completed}
+            />
+            <h5 className="text-xl font-bold">{todo.title}</h5>
+          </div>
+          <button onClick={() => handleDelete(todo.id)}>
+            <Trash2 size={18} className="ml-2 text-red-600" />
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TodoLists;
--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,13 +1,54 @@
 import Navbar from "@/components/Navbar";
+import { Todo } from "@/lib/types";
 import { Lock } from "lucide-react";
+import { verify } from "jsonwebtoken";
+import { cookies } from "next/headers";
+import { TOKEN_NAME, JWT_SECRET } from "@/components/constants/cookie";
+import axios from "axios";
+import TodoLists from "./TodoLists";
 
-const Todo = () => {
+const Todo = async () => {
+  let todoList: Todo[] = [];
+  let isLoggedIn = false;
+
+  const cookieStore = cookies();
+  const access_token = cookieStore.has(TOKEN_NAME);
+  const token = cookieStore.get(TOKEN_NAME);
+
+  if (access_token && token) {
+    const { value } = token;
+    try {
+      const payload = verify(value, JWT_SECRET);
+      if (typeof payload === "string") return;
+      isLoggedIn = true;
+      const { data } = await axios(
+        `https://jsonplaceholder.typicode.com/todos?userId=${payload.id}`
+      );
+      todoList = data;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
-    <main className="min-h-screen grid place-content-center">
-        <Navbar page={3} />
-      <h2 className="text-4xl text-amber-600 font-bold flex items-center">
-        Login to See Your Todo List <Lock className="ml-5" size={36} strokeWidth={2.5} />
-      </h2>
+    <main className="min-h-screen">
+      <Navbar page={3} />
+      {isLoggedIn ? (
+        <section className="w-4/5 mx-auto px-28 mb-32">
+          <h2 className="text-3xl font-bold text-center mb-20 mt-12 text-amber-600">
+            Your TodoLists
+          </h2>
+
+         <TodoLists todoList={todoList} />
+        </section>
+      ) : (
+        <div className="h-[90vh] w-full grid place-content-center">
+          <h2 className="text-4xl text-amber-600 font-bold flex items-center">
+            Login to See Your Todo List{" "}
+            <Lock className="ml-5" size={36} strokeWidth={2.5} />
+          </h2>
+        </div>
+      )}
     </main>
   );
 };
