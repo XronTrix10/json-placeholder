d48db5ea8334430f1b4cc7c0e5756026ff2f46cc
Xron Trix <xrontrix@gmail.com>
2024-08-26 01:07:57 +0530

    refactor: fixed loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import PageLoader from "@/components/loader/PageLoader";
+import { type FC } from "react";
+
+const loading: FC = () => {
+  return <PageLoader />;
+};
+
+export default loading;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,22 @@
+import { BACKEND_URL } from "@/components/constants/backend";
 import Navbar from "@/components/Navbar";
-import PageLoader from "@/components/loader/PageLoader";
 import PostGrid from "@/components/sections/PostGrid";
 import { type Post } from "@/lib/types";
 import axios from "axios";
-import { Suspense } from "react";
 
 export default async function Home() {
   let posts: Post[] = [];
 
   try {
-    const data = await axios("https://jsonplaceholder.typicode.com/posts");
+    const data = await axios(`${BACKEND_URL}/posts`);
     posts = data.data;
-    // console.log(data);
   } catch (error) {
     console.error(error);
   }
   return (
     <main className="flex">
       <Navbar page={0} />
-
-      {/* <Searchbar /> */}
-      <Suspense fallback={<PageLoader />}>
-        <PostGrid posts={posts} />
-      </Suspense>
+      <PostGrid posts={posts} />
     </main>
-    // <PageLoader />
   );
 }
