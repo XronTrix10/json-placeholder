8d7728431183aef52609a4100b2459be35a419d2
Xron Trix <xrontrix@gmail.com>
2024-10-09 11:56:30 +0530

    feat: random picsum image in gallery

--- src/components/sections/PhotoGrid.tsx
+++ src/components/sections/PhotoGrid.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { type Photo } from "@/lib/types";
+import { randomInt } from "crypto";
 import { useState } from "react";
 
 const PhotoGrid = (props: { photos: Photo[] }) => {
@@ -10,9 +11,9 @@ const PhotoGrid = (props: { photos: Photo[] }) => {
     <>
       <section className="w-4/5 mx-auto grid grid-cols-3 gap-4 2xl:gap-x-8 2xl:gap-y-6 mt-12">
         {props.photos.slice(0, displayPhotos).map((photo) => (
-          <div key={photo.id} className="bg-gray-200 p-2 rounded-lg">
+          <div key={photo.id} className="rounded-lg">
             <img
-              src={photo.thumbnailUrl}
+              src={`https://picsum.photos/id/${Math.floor((Math.random() * 98) + 1)}/800/500`}
               alt={photo.title}
               className="w-full h-48 2xl:h-56 object-cover rounded-lg"
             />
@@ -24,7 +25,7 @@ const PhotoGrid = (props: { photos: Photo[] }) => {
           onClick={() => {
             setDisplayPhotos(displayPhotos + 6);
           }}
-          className="p-2 px-4 bg-amber-400 rounded-full mx-auto hover:text-white font-bold hover:font-normal hover:bg-amber-500"
+          className="p-2 px-4 load-more rounded-full mx-auto font-bold"
         >
           Load More
         </button>
